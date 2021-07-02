35c0ab820762fa04a427cabc7e5c4c79d97ee6c9
Si Young Byun <siyoung.byun@gmail.com>
2021-07-02 00:09:56 -0500

    Implements rooms

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class Room(core_models.AbstractTimeStamp):
+    """Room Model Definition"""
+    # Basic information
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    # Location
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    address = models.CharField(max_length=140)
+    price = models.IntegerField()
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
